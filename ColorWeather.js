357de28b6ee47d16020d6e0e237d5e05fe586e7a
agesky <guzhu@msn.com>
2021-10-22 15:31:28 +0800

    Create ColorWeather.js
    彩云天气

---
+++ ColorWeather.js
@@ -0,0 +1,11 @@
+var body = $response['body'];
+var obj = JSON['parse'](body);
+obj['result']['xy_vip_expire'] = 0;
+obj['result']['is_vip'] = true;
+obj['result']['svip_expired_at'] = 4096483190;
+obj['result']['is_xy_vip'] = true;
+obj['result']['vip_type'] = 's';
+body = JSON['stringify'](obj);
+$done({
+    body
+})
