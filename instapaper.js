26926c434a43023dc83f72d6957c36c568e3439a
agesky <guzhu@msn.com>
2022-09-11 11:54:46 +0800

    Update instapaper.js

--- instapaper.js
+++ instapaper.js
@@ -8,6 +8,7 @@ hostname = www.instapaper.com
 
 */
 
+/*
 var body = $response.body;
 var objk = JSON.parse(body);
 
@@ -18,5 +19,23 @@ ojbk = {
 }
 
 body=JSON.stringify(objk);
+*/
 
-$done({body});
+
+body = $response.body.replace(/.+/g, 'isVip":true')
+
+function re() {  
+var body = $response.body;  
+if (arguments[0].includes("@")) {  
+ var regs = arguments[0].split("@");   
+var strs = arguments[1].split("@");   
+for (i = 0;i    
+ var reg = new RegExp(regs[i],"g");    // 另一种创建正则表达式的方式用 new RegExp构造函数   
+ body = body.replace(reg, strs[i]);  } }  
+else {   var reg = new RegExp(arguments[0],"g");  
+ body = body.replace(reg, arguments[1]); } 
+ $done(body);
+} 
+re('ipsubexp=@ipsub=\\d', 'ipsubexp=1746033923@ipsub=1') // 以设计让用户录入re('匹配1@匹配2@匹配3……', '替1@替2@替3……')，匹配中如果用到\d、\w等正则就以双斜杠出现。只替换一个则re('匹配1', '替1')
+
+
