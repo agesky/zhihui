969acc27064d955a207493ae2f3f01a1274083b1
agesky <guzhu@msn.com>
2022-09-11 09:24:43 +0800

    Update instapaper.js

--- instapaper.js
+++ instapaper.js
@@ -1,7 +1,7 @@
 /******************************
 
 [rewrite_local]
-^https:\/\/www\.instapaper\.com\/api\/subscription_status url script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/instapaper.js
+^https:\/\/www\.instapaper\.com\/api\/subscription_statusurl script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/instapaper.js
 
 [mitm]
 hostname = www.instapaper.com
@@ -9,6 +9,17 @@ hostname = www.instapaper.com
 *******************************/
 
 
-body = $response.body.replace(.+/g, '"ipsubnow=1588350628&ipsubexp=1800000000&ipsub=1"')
+# body = $response.body.replace(.+/g, '"ipsubnow=1588350628&ipsubexp=1800000000&ipsub=1"')
+
+var body = $response.body ;
+var objk = JSON.parse (body) ;
+
+ojbk = {
+    "ipsubnow" : 1588350628,
+    "ipsubexp" : 1800000000,
+    "ipsub" : 1
+    }
+
+body=JSON.stringify(objk);
 
 $done({body});
