25598ed21c49a3735da90242527789f52672f4db
agesky <guzhu@msn.com>
2022-09-12 23:10:10 +0800

    Update cubox.js

--- cubox.js
+++ cubox.js
@@ -10,6 +10,6 @@ var body = $response.body;
 var reg1 = /isExpire":\w+/g;
 var reg2 = /expireTime":"\w+/g;
 body = body.replace(reg1, 'isExpire":false');
-body = body.replace(reg2, 'expireTime":"2022-09-29T19:57:33+08:00');
+body = body.replace(reg2, 'expireTime":"2023');
 
 $done(body);
