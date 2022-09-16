465ab35840a024abdb6c03b85547034021fe5dca
agesky <guzhu@msn.com>
2022-09-16 20:21:36 +0800

    Create termuxl.js

---
+++ termuxl.js
@@ -0,0 +1,15 @@
+/*
+[rewrite_local]
+^ https://srmapp.com/purchases/validate* url script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/termuxl.js
+[mitm]
+hostname = srmapp.com
+*/
+
+var objc = JSON.parse($response.body);
+
+    objc = 
+{
+"status":"VALID"
+｝
+
+$done({body : JSON.stringify(objc)});
