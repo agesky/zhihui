97e6df63eff22ef31264fb23c7db740d54ae8985
agesky <guzhu@msn.com>
2022-10-11 11:52:19 +0800

    Create flomo.js

---
+++ flomo.js
@@ -0,0 +1,24 @@
+/*
+[rewrite_local]
+^https?:\/\/flomoapp\.com\/api\/v1\/user\/me\? url script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/flomo.js
+[mitm]
+hostname = flomoapp.com
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = 'api_key=flomoapp_ios';
+
+
+if (url.indexOf(vip) != -1) {
+    obj.data.pro_expired_at = "2099-10-01 00:00:00";
+
+
+
+	body = JSON.stringify(obj);
+}
+
+
+$done({body});
