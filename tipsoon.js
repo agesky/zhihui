e277adb6b65ed41655cf51a747e9418b73bac7be
agesky <guzhu@msn.com>
2022-11-15 18:54:38 +0800

    Update tipsoon.js

--- tipsoon.js
+++ tipsoon.js
@@ -7,19 +7,15 @@ hostname = ios.tipsoon.com
 
 
 var body = $response.body;
-var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = 'c=api4';
 
 
-if (url.indexOf(vip) != -1) {
     obj.user.vip_expire_time = "2099-10-01 00:00:00";
     obj.user.is_vip = 1;
 
 
-	body = JSON.stringify(obj);
-}
+    body = JSON.stringify(obj);
 
 
 $done({body});
