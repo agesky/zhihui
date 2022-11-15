ece575d810f0a0612c062f923f8144c970a3a804
agesky <guzhu@msn.com>
2022-11-15 19:15:49 +0800

    Update tipsoon.js

--- tipsoon.js
+++ tipsoon.js
@@ -1,6 +1,6 @@
 /*
 [rewrite_local]
-^https?:\/\/api\.tipsoon\.com\/api\/v1\/user\/info  url script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/tipsoon.js
+^https?:\/\/api\.tipsoon\.com\/api\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/tipsoon.js
 [mitm]
 hostname = api.tipsoon.com
 */
@@ -13,7 +13,7 @@ var obj = JSON.parse(body);
 ｛
     obj.data.vip_expire_time = "2099-10-01 00:00:00";
     obj.data.is_vip = true;
-    obj.data.is_red = 1;
+    obj.data.is_red = "1";
 
     body = JSON.stringify(obj);
 
