331b402d7d9359fd36c57947090fbf015edefa63
agesky <guzhu@msn.com>
2024-01-14 13:10:08 +0800

    Update shanqiu.js

--- shanqiu.js
+++ shanqiu.js
@@ -1,6 +1,6 @@
 /*
 [rewrite_local]
-^http?:\/\/i.815616\.xyz\/api\/v2\/myinfo\/* url script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/shanqiu.js
+^http?:\/\/*.815616\.xyz\/api\/v2\/myinfo\/* url script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/shanqiu.js
 [mitm]
 hostname = 815616.xyz
 */
