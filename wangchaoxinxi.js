17c7d0d073e21ac196a5f37333d92fa6d257c66e
agesky <guzhu@msn.com>
2024-06-23 10:54:39 +0800

    更新 wangchaoxinxi.js

--- wangchaoxinxi.js
+++ wangchaoxinxi.js
@@ -1,3 +1,11 @@
+/*
+[rewrite_local]
+^https:\/\/wangchaoxinxi\.cn\/wechat\/i\/？ url script-response-body https://raw.githubusercontent.com/agesky/zhihui/master/wangchaoxinxi.js
+[mitm]
+hostname = wangchaoxinxi.cn
+*/
+
+
 var body = $response.body;
 body = body.replace('</body>', '<script>document.getElementById("tssfPayDiv").style.display = "none";</script></body>');
 $done({body});
