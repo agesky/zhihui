3f9dd9cc7c4420e1d6f5641ed034fd8b33055387
agesky <guzhu@msn.com>
2025-06-20 17:46:41 +0800

    Update weidian_cookie.js

--- weidian_cookie.js
+++ weidian_cookie.js
@@ -3,7 +3,7 @@
  */
 /*
 [rewrite_local]
-^https:\/\/*\.weidian\.com\/* script-request-header https://raw.githubusercontent.com/agesky/zhihui/master/weidian_cookie.js
+^https:\/\/*\.weidian\.com\/* url script-request-header https://raw.githubusercontent.com/agesky/zhihui/master/weidian_cookie.js
 [mitm]
 hostname = *.weidian.com
 */
