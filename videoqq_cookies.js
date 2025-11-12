26d7885534ce0f6a83b37996c38cdcf7e122cd6f
agesky <guzhu@msn.com>
2025-11-13 07:13:03 +0800

    更新 videoqq_cookies.js

--- videoqq_cookies.js
+++ videoqq_cookies.js
@@ -1,7 +1,7 @@
 /*
 [rewrite_local]
 # 腾讯视频 cookie1
-^https?:\/\/vip.video.qq.com/rpc/trpc.new_task_system.task_system.TaskSystem/ReadTaskList? url script-request-header https://raw.githubusercontent.com/ClydeTime/Quantumult/main/Script/Task/videoqq.js
+^https?:\/\/vip.video.qq.com/rpc/trpc.query_vipinfo.vipinfo.QueryVipInfo/GetVipUserInfoH5? url script-request-header https://raw.githubusercontent.com/ClydeTime/Quantumult/main/Script/Task/videoqq.js
 
 [mitm]
 # 必需的主机名
