c74f039901952a7ed39b7a4b1bf2bf5704960f5c
agesky <guzhu@msn.com>
2025-11-13 07:07:33 +0800

    创建 videoqq_cookies.js

---
+++ videoqq_cookies.js
@@ -0,0 +1,9 @@
+/*
+[rewrite_local]
+# 腾讯视频 cookie1
+^https?:\/\/vip.video.qq.com/rpc/trpc.new_task_system.task_system.TaskSystem/ReadTaskList? url script-request-header https://raw.githubusercontent.com/ClydeTime/Quantumult/main/Script/Task/videoqq.js
+
+[mitm]
+# 必需的主机名
+hostname = vip.video.qq.com
+*/
